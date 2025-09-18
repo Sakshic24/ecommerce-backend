e16b38ea4e07b1bb2bfc1be70ed0d8b1a198907d
Sakshi Chaurasiya <sakshichaurasia66@gmail.com>
2025-09-18 19:13:51 +0530

    backend ready for deploy

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import cors from "cors";
 import Razorpay from "razorpay";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
@@ -19,6 +21,9 @@ app.use(cors());
 //   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 // });
 
+console.log(process.env.RAZORPAY_KEY_ID);
+console.log(process.env.RAZORPAY_SECRET);
+
 export const instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET,
@@ -29,6 +34,7 @@ import userRouter from "./routes/user.routes.js";
 import categoryRouter from "./routes/category.routes.js";
 import productRouter from "./routes/product.routes.js";
 import paymentRouter from "./routes/payment.routes.js";
+import { log } from "console";
 
 //app router declaation
 app.use("/api/v1/users", userRouter);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-dotenv.config({ path: "../.env" });
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+
+// dotenv.config({ path: "../.env" });
+dotenv.config();
 
 connectDB()
   .then(() => {
